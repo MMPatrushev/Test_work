01e4d66062856f8cda9399cda3f7fd19fb64eb99
Mikhail <mhlmhlpatrushev@gmail.com>
2017-02-16 01:34:06 +0300

    first and last commit

---
+++ Testclass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+
+/// <summary>
+/// Сводное описание для Testclass
+/// </summary>
+public class Testclass
+{
+    public class Posts()
+    {
+        private integer id_post;
+
+    }
+    public static void SendMail()
+    {
+ 
+    }
+	public Testclass()
+	{
+		
+	}
+}
